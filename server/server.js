d49e6691d2b2eab1c0c038cb2c911f14d5d9a6cd
monkJs <admin@monkjs.com>
2017-12-10 19:41:04 -0800

    Init

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
+const path = require('path');
+const http = require('http');
+const app = express();
+const cors = require('cors')
+var server = http.createServer(app);
+app.use(morgan('combined'));
+app.use(cors()); // Allows CORS(Cross Origin GET Request)
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json({type: '*/*'}));
+app.use("/", express.static(publicPath));
+app.listen(port, function() {
+  console.log('Server Listening on localhost:' + port);
+});
+
+module.exports = app;
